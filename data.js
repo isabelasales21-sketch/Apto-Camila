a10af21256efabef2289ef8c33d4c0d1c4a89593
isabelasales21-sketch <isabela.sales21@gmail.com>
2026-04-13 20:01:17 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,199 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sala-cozinha_360",
+      "name": "SALA-COZINHA_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4878175262521598,
+          "pitch": 0.44711075617207996,
+          "rotation": 0,
+          "target": "4-banho-social_360"
+        },
+        {
+          "yaw": -1.6684199444837606,
+          "pitch": 0.49172616805211966,
+          "rotation": 4.71238898038469,
+          "target": "3-visitas---360"
+        },
+        {
+          "yaw": 2.268682574090123,
+          "pitch": 0.694023760935826,
+          "rotation": 4.71238898038469,
+          "target": "1-suite---360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-suite---360",
+      "name": "SUITE - 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 775,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4195935672533153,
+          "pitch": 0.5576254071724698,
+          "rotation": 0,
+          "target": "2-banho-sute_360"
+        },
+        {
+          "yaw": -2.1709455738569954,
+          "pitch": 0.5649056786283069,
+          "rotation": 4.71238898038469,
+          "target": "0-sala-cozinha_360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-banho-sute_360",
+      "name": "BANHO SUÍTE_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8718868078250486,
+          "pitch": 1.2866996891913178,
+          "rotation": 0,
+          "target": "1-suite---360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-visitas---360",
+      "name": "VISITAS - 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.7047191609951247,
+        "pitch": 0.4292785690057741,
+        "fov": 1.0206223595689021
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9049948566090862,
+          "pitch": 0.3907826713085445,
+          "rotation": 0,
+          "target": "0-sala-cozinha_360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-banho-social_360",
+      "name": "BANHO SOCIAL_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5472658406684943,
+          "pitch": 1.4076511885180096,
+          "rotation": 0,
+          "target": "0-sala-cozinha_360"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
