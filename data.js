456ccd4e28767c9c647dfd8033719e5090e450d5
isabelasales21-sketch <isabela.sales21@gmail.com>
2026-04-13 21:27:17 -0300

    oi

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-sala-cozinha_360",
-      "name": "SALA-COZINHA_360",
+      "id": "0-sala-cozinha",
+      "name": "SALA-COZINHA",
       "levels": [
         {
           "tileSize": 256,
@@ -16,9 +16,63 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 1024,
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5227776743731702,
+        "pitch": 0.12395525516650707,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4961522222015464,
+          "pitch": 0.43957898226145886,
+          "rotation": 0,
+          "target": "4-banho-social"
+        },
+        {
+          "yaw": -1.6590523079082793,
+          "pitch": 0.4859508740153764,
+          "rotation": 5.497787143782138,
+          "target": "3-quarto-visita"
+        },
+        {
+          "yaw": 2.273224796809176,
+          "pitch": 0.7239985785003054,
+          "rotation": 0,
+          "target": "1-sute"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sute",
+      "name": "SUÍTE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
@@ -26,29 +80,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.4878175262521598,
-          "pitch": 0.44711075617207996,
+          "yaw": -1.4175712111346321,
+          "pitch": 0.5775532221252568,
           "rotation": 0,
-          "target": "4-banho-social_360"
+          "target": "2-banho-sute"
         },
         {
-          "yaw": -1.6684199444837606,
-          "pitch": 0.49172616805211966,
+          "yaw": -2.1122886181170593,
+          "pitch": 0.5794094081760086,
           "rotation": 4.71238898038469,
-          "target": "3-visitas---360"
-        },
-        {
-          "yaw": 2.268682574090123,
-          "pitch": 0.694023760935826,
-          "rotation": 4.71238898038469,
-          "target": "1-suite---360"
+          "target": "0-sala-cozinha"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-suite---360",
-      "name": "SUITE - 360",
+      "id": "2-banho-sute",
+      "name": "BANHO-SUÍTE",
       "levels": [
         {
           "tileSize": 256,
@@ -62,9 +110,13 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 775,
+      "faceSize": 2048,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
@@ -72,23 +124,17 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.4195935672533153,
-          "pitch": 0.5576254071724698,
+          "yaw": 1.9929874414427804,
+          "pitch": 1.3026600850459644,
           "rotation": 0,
-          "target": "2-banho-sute_360"
-        },
-        {
-          "yaw": -2.1709455738569954,
-          "pitch": 0.5649056786283069,
-          "rotation": 4.71238898038469,
-          "target": "0-sala-cozinha_360"
+          "target": "1-sute"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-banho-sute_360",
-      "name": "BANHO SUÍTE_360",
+      "id": "3-quarto-visita",
+      "name": "QUARTO-VISITA",
       "levels": [
         {
           "tileSize": 256,
@@ -102,9 +148,13 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 1024,
+      "faceSize": 2048,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
@@ -112,17 +162,17 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.8718868078250486,
-          "pitch": 1.2866996891913178,
+          "yaw": 1.9157553991974465,
+          "pitch": 0.4210267221688717,
           "rotation": 0,
-          "target": "1-suite---360"
+          "target": "0-sala-cozinha"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-visitas---360",
-      "name": "VISITAS - 360",
+      "id": "4-banho-social",
+      "name": "BANHO-SOCIAL",
       "levels": [
         {
           "tileSize": 256,
@@ -136,43 +186,13 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "yaw": 1.7047191609951247,
-        "pitch": 0.4292785690057741,
-        "fov": 1.0206223595689021
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.9049948566090862,
-          "pitch": 0.3907826713085445,
-          "rotation": 0,
-          "target": "0-sala-cozinha_360"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-banho-social_360",
-      "name": "BANHO SOCIAL_360",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
         },
         {
           "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
+          "size": 2048
         }
       ],
-      "faceSize": 1024,
+      "faceSize": 2048,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
@@ -180,16 +200,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.5472658406684943,
-          "pitch": 1.4076511885180096,
+          "yaw": -2.466546426619459,
+          "pitch": 1.4255939823182784,
           "rotation": 0,
-          "target": "0-sala-cozinha_360"
+          "target": "0-sala-cozinha"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "APTO CAMILA",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
